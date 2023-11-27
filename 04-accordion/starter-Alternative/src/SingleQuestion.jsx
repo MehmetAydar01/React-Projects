455834def8e12cc01bf5aac543988dbfb52b42b1
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-11-27 19:44:19 +0300

    component created,icons added and click func operation

---
+++ 04-accordion/starter-Alternative/src/SingleQuestion.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const SingleQuestion = ({ id, title, info, activeId, toggleQuestion }) => {
+  const isActive = id === activeId;
+
+  return (
+    <article className='question'>
+      <header>
+        <h5>{title}</h5>
+        <button
+          type='button'
+          className='question-btn'
+          onClick={() => toggleQuestion(id)}
+        >
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isActive && <p>{info}</p>}
+    </article>
+  );
+};
+
+export default SingleQuestion;
