11da8f88d01b70f6eacc33d87e7545114aa9448d
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-12-11 23:19:21 +0300

    Navbar Comp created

---
+++ 13-strapi-submenus/starter/src/Navbar.jsx
@@ -0,0 +1,27 @@
+import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+import NavLinks from './NavLinks';
+
+const Navbar = () => {
+  const { openSidebar, setPageId } = useGlobalContext();
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('nav-link')) {
+      setPageId(null);
+    }
+  };
+
+  return (
+    <nav onMouseOver={handleSubmenu}>
+      <div className='nav-center'>
+        <h3 className='logo'>strapi</h3>
+        <button type='button' className='toggle-btn' onClick={openSidebar}>
+          <FaBars />
+        </button>
+        <NavLinks />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
