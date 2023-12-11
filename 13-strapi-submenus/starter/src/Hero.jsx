eca97e13151b0dc587ec44419113891cbf00775e
MehmetAydar01 <mehmet.aydar07@gmail.com>
2023-12-11 23:19:15 +0300

    Hero comp created

---
+++ 13-strapi-submenus/starter/src/Hero.jsx
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Strapi is the loading open-source headless CMS. It's 100% Javascript
+          and fully customizable.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
